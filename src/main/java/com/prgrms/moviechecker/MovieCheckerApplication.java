4fd816e17a148aac46603793550f99ac5879af04
hyee0715 <hyee0715@naver.com>
2023-08-18 16:42:25 +0900

    Initial commit

---
+++ src/main/java/com/prgrms/moviechecker/MovieCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.prgrms.moviechecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCheckerApplication.class, args);
+	}
+
+}
